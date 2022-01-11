46e8905309fdc12ad51aa1cbfa0d1f7b07e033a2
Guilherme Duarte <gui.duarte123@outlook.com>
2022-01-10 21:58:51 -0300

    adiconada nova

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-echo "hello word";
-echo "meu nome e Guilherme "
-echo "sou aprendiz "
+echo "<h1>hello word</h1>";
+echo "meu nome e Guilherme<br>";   
+echo "sou aprendiz ";
+print "<p> segundo paragrafo </p>";
 ?>
